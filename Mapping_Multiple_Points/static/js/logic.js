9ac6d7ea41b9177944c31e232a6b46ae0e029cff
TanvirZulkarnaine <tanvir7.rahman@gmail.com>
2022-11-26 00:08:49 -0700

    Added code to view multiple points on map

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,19 +4,27 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid").setView([40.7, -94.5], 4);
 
-//  Add a circle to the map for Los Angeles, California.
-// Note: Marker is a circle with a radius of 100 meters. 
-// L.circle([34.0522, -118.2437], {
-//   radius: 100,
-// }).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
-// Add a circle to the map using circleMarker()
-// Note: Marker is a circle with a radius of 300 PIXELS!
-L.circleMarker([34.0522, -118.2437],{
-  radius: 300,
-  color: "black",
-  fillColor: '#ffffa1'
-}).addTo(map);
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city){
+//   console.log(city)
+//   L.marker(city.location).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map)
+// });
+
+// Loop through the cityData array and create one circle marker for each city.
+// Note: radius is scaled according to city's population
+cityData.forEach(function(city){
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "orange",
+    lineweight: 4
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map)
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
